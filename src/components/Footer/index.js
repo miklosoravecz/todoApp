eec29a058539c2e52978653f82a683ae56b57a12
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-26 19:39:04 +0300

    footer & header components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+	return (
+		<div className="footer">
+			<h2>Miklós Oravecz 2022 &copy;</h2>
+		</div>
+	)
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Header.css"
+
+export const Header = () => {
+	return (
+		<header className="header">
+			<h1 className="header__text">Todo App</h1>
+		</header>
+	)
+}
