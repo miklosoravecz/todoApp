a55f74b94c5519eafc26d3ae5328e824b68a54ed
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-27 19:55:02 +0300

    item and styles created

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,6 +1,30 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import "./Item.css"
 
-export const Item = () => {
-	return <div className="item"></div>
+export const Item = props => {
+	const [status, setStatus] = useState(props.status)
+
+	useEffect(() => {
+		statusSelector()
+	})
+
+	const statusSelector = () => {
+		if (props.status === "Done") {
+			setStatus("is-success is-light")
+		} else if (props.status === "In progress") {
+			setStatus("is-warning is-light")
+		} else {
+			setStatus("is-danger is-light")
+		}
+	}
+
+	return (
+		<li className="item">
+			<div className={`item-wrapper notification ${status}`}>
+				<h3 className="item-title">{props.title}</h3>
+				<p className="item-deadline-text">Deadline: {props.deadline}</p>
+				<p>Status: {props.status}</p>
+			</div>
+		</li>
+	)
 }
