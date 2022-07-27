df256bdc55ba5f638baf156f31cd0cbaca143bd8
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-27 17:40:28 +0300

    modal added

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Button} from "../Button"
 import {Footer} from "../Footer/index"
 import {Header} from "../Header/index"
+import {Modal} from "../Modal"
 import "./Page.css"
 
 export const Page = () => {
@@ -9,7 +10,14 @@ export const Page = () => {
 		<div className="page">
 			<Header />
 			<main className="main">
+				<ul>
+					<h4 className="list-text">Going to shop</h4>
+				</ul>
+				<ul>
+					<h4 className="list-text">Cooking soup</h4>
+				</ul>
 				<Button>Add a new todo!</Button>
+				<Modal />
 			</main>
 			<Footer />
 		</div>
