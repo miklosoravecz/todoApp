ced75d5b67adcf2487653111657edec46fb8ab58
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-27 19:55:17 +0300

    list and styles created

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,6 +1,15 @@
 import React from "react"
+import {Item} from "../Item"
 import "./List.css"
 
 export const List = () => {
-	return <div className="list"></div>
+	return (
+		<div className="list">
+			<ul>
+				<Item title="new title" deadline="after tomorrow" status="Done" />
+				<Item title="new title2" deadline="tomorrow" status="In progress" />
+				<Item title="new title3" deadline="3 days later" status="Not started" />
+			</ul>
+		</div>
+	)
 }
