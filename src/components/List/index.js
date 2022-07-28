16618a71d65f3aac9d751e3ed833aee2cea8e52b
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-28 16:39:42 +0300

    iteration with map method

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,29 +2,18 @@ import React from "react"
 import {Item} from "../Item"
 import "./List.css"
 
-export const List = () => {
+export const List = props => {
+	const items = props.listItems.map(item => (
+		<Item
+			id={item.id}
+			title={item.title}
+			status={item.status}
+			deadline={item.deadline}
+		/>
+	))
 	return (
 		<div className="list">
-			<ul>
-				<Item
-					title="new title"
-					deadline="after tomorrow"
-					status="Done"
-					id="id1"
-				/>
-				<Item
-					title="new title2"
-					deadline="tomorrow"
-					status="In progress"
-					id="id2"
-				/>
-				<Item
-					title="new title3"
-					deadline="3 days later"
-					status="Not started"
-					id="id3"
-				/>
-			</ul>
+			<ul>{items}</ul>
 		</div>
 	)
 }
