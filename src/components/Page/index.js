df1d7832e5bf3cd61a43f9a37c54f6985b6fcfe0
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-28 20:15:01 +0300

    uniqe id added

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,29 +1,38 @@
-import React, {useEffect, useState} from "react"
+import React, {useState} from "react"
 import {Button} from "../Button"
 import {Footer} from "../Footer/index"
 import {Header} from "../Header/index"
 import {List} from "../List"
 import {Modal} from "../Modal"
 import "./Page.css"
+import {v4 as uuidv4} from "uuid"
 
 export const Page = () => {
 	const [list, setList] = useState([])
+	const [validationError, setValidationError] = useState(null)
 
 	const addItem = (title, deadline, status) => {
-		const newItem = {id: "id", title: title, deadline: deadline, status: status}
+		if (title.length === 0 || deadline.length === 0) {
+			setValidationError("Title and deadline must be filled!")
+			return
+		}
 
+		const newItem = {
+			id: uuidv4(),
+			title: title,
+			deadline: deadline,
+			status: status
+		}
 		setList([...list, newItem])
+		setValidationError(null)
 	}
 
-	useEffect(() => {
-		setList(list)
-	}, [list])
 	return (
 		<div className="page">
 			<Header />
 			<main className="page-main">
 				<Button cls="page-add-btn">Add a new todo!</Button>
-				<Modal addItem={addItem} />
+				<Modal addItem={addItem} validationError={validationError} />
 				<List list={list} />
 			</main>
 			<Footer />
