9f8bfb1fd771dba39d85b78f127d0b5e817e2144
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-28 17:32:29 +0300

    handleChange,handleSubmit and useState

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 import "./Form.css"
 
-export const Form = () => {
+export const Form = props => {
+	const [title, setTitle] = useState("Title")
+	const [deadline, setDeadline] = useState("Deadline")
+	const [status, setStatus] = useState("Not started")
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		props.addItem(title, deadline, status)
+		setTitle("Title")
+		setDeadline("Deadline")
+		setStatus("Status")
+	}
+
+	function handleTitleChange(e) {
+		setTitle(e.target.value)
+	}
+
+	function handleDeadlineChange(e) {
+		setDeadline(e.target.value)
+	}
+
+	function handleStatusChange(e) {
+		setStatus(e.target.value)
+	}
+
 	return (
 		<form className="form">
 			<input
@@ -9,17 +33,24 @@ export const Form = () => {
 				id="title"
 				className="input"
 				name="Title"
-				placeholder="Title"
+				value={title}
+				onChange={handleTitleChange}
 			/>
 			<input
 				type="text"
 				id="deadline"
 				className="input"
 				name="Deadline"
-				placeholder="Deadline"
+				value={deadline}
+				onChange={handleDeadlineChange}
 			/>
 			<div className="form-buttons">
-				<select className="form-select" name="Status" id="status">
+				<select
+					className="form-select"
+					name="Status"
+					id="status"
+					onChange={handleStatusChange}
+				>
 					<option value="" disabled selected>
 						Status
 					</option>
@@ -31,7 +62,7 @@ export const Form = () => {
 					<button type="submit" className="button">
 						Cancel
 					</button>
-					<button type="submit" className="button">
+					<button type="submit" className="button" onClick={handleSubmit}>
 						Add
 					</button>
 				</div>
