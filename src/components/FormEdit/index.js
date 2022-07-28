06e804994a21bcd92f1e98c1c9a885f3f010e6a1
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-28 22:54:10 +0300

    FormEdit component created

---
+++ src/components/FormEdit/index.js
@@ -0,0 +1,79 @@
+import React, {useState} from "react"
+import {Button} from "../Button"
+import "./FormEdit.css"
+
+export const FormEdit = props => {
+	const [title, setTitle] = useState("Title")
+	const [deadline, setDeadline] = useState("Deadline")
+	const [status, setStatus] = useState("Not started")
+	const [value, setValue] = useState("default")
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		props.editItem(props.id, title, deadline, status)
+		setTitle("Title")
+		setDeadline("Deadline")
+		setStatus("Not started")
+		setValue("default")
+		console.log("clicked in edit item")
+	}
+
+	const handleTitleChange = e => {
+		setTitle(e.target.value)
+	}
+
+	const handleDeadlineChange = e => {
+		setDeadline(e.target.value)
+	}
+
+	const handleStatusChange = e => {
+		setStatus(e.target.value)
+		setValue(e.target.value)
+	}
+
+	return (
+		<form className="form-edit">
+			<input
+				type="text"
+				id="title"
+				className="input"
+				name="Title"
+				value={title}
+				onChange={handleTitleChange}
+			/>
+			<input
+				type="text"
+				id="deadline"
+				className="input"
+				name="Deadline"
+				value={deadline}
+				onChange={handleDeadlineChange}
+			/>
+			<div className="form-edit-buttons">
+				<select
+					className="form-edit-select"
+					name="Status"
+					id="status"
+					onChange={handleStatusChange}
+					value={value}
+				>
+					<option value="default" disabled selected>
+						Status
+					</option>
+					<option value="In progress">In progress</option>
+					<option value="Not started">Not started</option>
+					<option value="Done">Done</option>
+				</select>
+				<div className="button-wrapper">
+					<Button type="submit" className="button">
+						Cancel
+					</Button>
+					<Button type="submit" className="button" func={handleSubmit}>
+						Add
+					</Button>
+				</div>
+			</div>
+		</form>
+	)
+}
