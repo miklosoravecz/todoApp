865c768293a2808b9fe5f0678976b0db4b3de6ea
Miklos Oravecz <oravecz.miklos@gmail.com>
2022-07-28 17:32:03 +0300

    addItem props passed in

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,11 +2,11 @@ import React from "react"
 import {Form} from "../Form"
 import "./Modal.css"
 
-export const Modal = () => {
+export const Modal = props => {
 	return (
 		<div className="modal-wrapper">
 			<h2 className="form-title">Add a new todo to the list</h2>
-			<Form />
+			<Form addItem={props.addItem} />
 		</div>
 	)
 }
